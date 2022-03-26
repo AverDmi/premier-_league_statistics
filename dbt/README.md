a252d9e2f64948bc233d446a84d3973452075921
Dmitriy Averyanov <dimthomas1989@gmail.com>
2022-03-26 17:50:17 +0300

    add dbt project on master branch

---
+++ dbt/README.md
@@ -0,0 +1,1 @@
+# premier-_league_statistics
---
+++ dbt/models/staging/stg_football_statistics.sql
@@ -0,0 +1,35 @@
+{{ config(materialized='view') }}
+
+select
+    {{ get_date(Date) }} as match_date,
+    cast(Time as TIME FORMAT 'HH24:MI') as match_time,
+    HomeTeam,
+    AwayTeam,
+    FTHG,
+    FTAG,
+    {{ get_result_description_FTR('FTR') }} as FTR,
+    HTHG,
+    HTAG,
+    {{ get_result_description_HTR('HTR') }} as HTR,
+    Referee,
+    HS,
+    premier_league_statistics.AS,
+    HST,
+    AST,
+    HF,
+    AF,
+    HC,
+    AC,
+    HY,
+    AY,
+    HR,
+    AR
+from {{ source('staging', 'premier_league_statistics') }}
+where FTR is not Null
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
